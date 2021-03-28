1d387f5b5a188b76c76768fec35379f8c432c743
lianjiexue <1711861430@qq.com>
2021-03-28 19:14:40 +0800

    30

--- main.go
+++ main.go
@@ -6,17 +6,27 @@ import (
 	"encoding/json"
 	"log"
 	"github.com/gorilla/websocket"
+	"time"
+	"math/rand"
 )
 var upgrader = websocket.Upgrader{
 	ReadBufferSize: 1024,
 	WriteBufferSize: 1024,
 }
+type Client struct {
+	Conn  *websocket.Conn
+	Uid int
+	Username string
+}
 var err error
+var clients map[*Client]bool
 func init(){
 	log.Println("runing 127.0.0.1:8080")
+	clients = make(map[*Client]bool)
 }
 
 func main(){
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		http.ServeFile(w,r,"index.html")
 	})
@@ -32,19 +42,38 @@ func main(){
 	})
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request){
 		conn,err := upgrader.Upgrade(w,r,nil)
-		if err != nil{}
-
+		if err != nil{
+			return
+		}
+		log.Println(time.Now())
+		client := new(Client)
+		client.Uid = rand.Intn(1000)
+		client.Conn = conn
+		clients[client] = true
 		for {
 			//读数据
 			messagetype,data,err := conn.ReadMessage()
 			
-			if err != nil{}
-
+			if err != nil{
+				log.Println("断开链接")
+				for client,_ := range clients{
+					if client.Conn == conn {
+						delete(clients,client)
+					}
+				}
+				delete(clients,client)
+				return
+			}
+			log.Println(len(clients))
+			log.Println(client)
 			//写数据
 
 			err = conn.WriteMessage(messagetype,data)
 
-			if err != nil{}
+			if err != nil{
+				log.Println("发送失败")
+				return
+			}
 		}
 
 	})
