fcff724f320ddb6aed9db821e0101777a74b66da
lianjiexue <1711861430@qq.com>
2021-05-10 01:57:36 +0800

    修改已读的bug

--- model/mood.go
+++ model/mood.go
@@ -27,6 +27,20 @@ func (m *Mood) TableName() string {
 func (mt *MoodTime) TableName() string {
 	return "gc_mood"
 }
+func GetFUllMoods(uid int, page int) []MoodTime {
+	var moods []Mood
+	var newmoods []MoodTime
+
+	offset := (page - 1) * 10
+	db.Where("uid", uid).Select("id", "content", "time").Limit(10).Offset(offset).Order("id DESC").Find(&moods)
+	var temp MoodTime
+	for _, value := range moods {
+		temp.MoodMixin = value.MoodMixin
+		temp.DateTime = time.Unix(int64(value.Time), 0).Format("2006.1.2 15:04")
+		newmoods = append(newmoods, temp)
+	}
+	return newmoods
+}
 func AddMood(uid int, content string) bool {
 	var mood Mood
 
@@ -41,6 +55,10 @@ func OneMood() MoodTime {
 	var moodtime MoodTime
 	//读取记录
 	db.Where("is_read=0").First(&mood)
+
+	if mood.Id != 0 {
+		db.Model(&Mood{}).Where("id", mood.Id).Update("is_read", 1)
+	}
 	moodtime.MoodMixin = mood.MoodMixin
 	moodtime.DateTime = time.Unix(int64(mood.Time), 0).Format("2006.1.2")
 	return moodtime
